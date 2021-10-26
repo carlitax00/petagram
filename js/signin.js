584256e1c5479d6c3a4db93e6d570cdfd3778036
carlitax00 <carlu.petta@hotmail.com>
2021-10-25 23:32:28 -0300

    Sign in page 25/10/21

--- js/signin.js
+++ js/signin.js
@@ -1,18 +1,78 @@
-addEventListener('DOMContentLoaded', () => {
-    const form_login = document.querySelector('#loginform')
-    const login_span = form_login.querySelector('.signinbtntext')
-    const loader = form_login.querySelector('#loader')
-    const errormessage = document.querySelector('containererror')
+const form = document.getElementById("form");  
+const username = document.getElementById("username");  
+const email = document.getElementById("email");  
+const password = document.getElementById("password");  
+const cPassword = document.getElementById("c-password");  
 
-    if(form_login) {
-        form_login.addEventListener('submit', event => {
-            event.preventDefault()
+/***** Mostrar mensaje de error *****/
+function showError(input, message) {  
+    const formControl = input.parentElement;  
+    formControl.className = "input error";  
+    const small = formControl.querySelector("small");  
+    small.innerText = message;  
+}  
 
-            const usuario = form_login.querySelector('.user').value
-            const password = form_login.querySelector('.password').value
+/***** Mostrar mensaje OK *****/
+function showSuccess(input) {  
+    const formControl = input.parentElement;  
+    formControl.classList.add("success");  
+}  
 
-            login_span.srtyle.display = 'none'
-            loader.style.display = 'inline'
-        })
-    }
-})
+/***** Verificar los campos *****/
+function checkRequired(inputArr) {  
+    inputArr.forEach(function (input) {  
+        if (input.value.trim() === "") {  
+            showError(input, `${getFieldName(input)} es requerido`);  
+        } else {  
+            showSuccess(input);  
+        }  
+    });  
+}  
+
+/***** Captar el mensaje de los campos *****/
+function getFieldName(input) {  
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);  
+}  
+
+/***** Validar la longitud del valor ingresado *****/
+function checkLenghth(input, min, max) {  
+    if (input.value.length < min) {  
+        showError(  
+            input,  
+            `${getFieldName(input)} debe tener al menos ${min} caracteres`
+            );  
+        } else if (input.value.length > max) {  
+            showError(  
+                input,  
+                `${getFieldName(input)} debe tener menos de ${max} caracteres`
+                );  
+            } else {  
+                showSuccess(input);  
+            }  
+        }  
+
+/***** Validar email *****/
+function checkEmail(input) {  
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+    if (re.test(input.value.trim())) {  
+        showSuccess(input);  
+    } else {  
+        showError(input, "La dirección de email no es válida");  
+    }  
+}  
+
+/***** Verificar que las contraseñas coincidan *****/
+function checkPasswordMatch(input1, input2) {  
+    if (input1.value !== input2.value) {  
+        showError(input2, "Passwords do not match");  
+    }  
+}  
+
+form.addEventListener("submit", (e) => {  
+    e.preventDefault();  
+    checkRequired([username, email, password, cPassword]);  
+    checkLenghth(username, 3, 15);  
+    checkLenghth(password, 8, 25);  
+    checkEmail(email);  
+    checkPasswordMatch(password, cPassword);  
+});
